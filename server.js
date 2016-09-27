43f38bffb3a09ad517541357fa481964ce99510d
nbappi <bappi.cse562@gmail.com>
2016-09-27 11:41:26 +0600

    REST server listening

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log("server js");
+var express = require("express");
+var http       = require("http");
+var app = express();
+var server     = http.createServer(app);
+var port = process.env.PORT || 3000;
+
+//listen
+server.listen(port, function () {
+	console.log("REST server listening on port : "+  port);
+});
